35bc7d53bbd9d60be034a75efa6d40ca3121e57f
Lurgrid <Lurgrid@gmail.com>
2023-12-08 22:06:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php
-        header("Refresh: 1; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+</head>
+</html>
